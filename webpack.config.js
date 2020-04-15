2c5b60cea230a9d9e0b191e9843ecbb0dba61a1a
TATA <evadcmd@gmail.com>
2020-04-16 02:02:37 +0900

    [ADD]create env

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: 'node_modules',
+                loader: 'babel-loader',
+            }
+        ]
+    }
+};
